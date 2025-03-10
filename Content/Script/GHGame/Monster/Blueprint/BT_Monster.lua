24c3dd566a39caf18acd56a35e2142414293d51c
2225244153 <2225244153@qq.com>
2025-03-10 17:07:59 +0800

    稍微有点修改

--- Content/Script/GHGame/Monster/Blueprint/BT_Monster.lua
+++ Content/Script/GHGame/Monster/Blueprint/BT_Monster.lua
@@ -4,10 +4,15 @@
 -- @COMPANY **
 -- @AUTHOR **
 -- @DATE ${date} ${time}
---
+-- 
 
 ---@type BP_Monster_C
 local M = UnLua.Class()
+
+package.cpath = package.cpath .. ';C:/Users/Administrator/AppData/Roaming/JetBrains/Rider2024.3/plugins/EmmyLua/debugger/emmy/windows/x64/?.dll'
+local dbg = require('emmy_core')
+dbg.tcpConnect('localhost', 9966)
+
 
 -- function M:Initialize(Initializer)
 -- end
@@ -15,9 +20,9 @@ local M = UnLua.Class()
 -- function M:UserConstructionScript()
 -- end
 
---[[function M:ReceiveBeginPlay()
-   
-end]]
+function M:ReceiveBeginPlay()
+   self:GetParentActor():ReceiveBeginPlay()
+end
 
 --[[function M:OnComponentActivated()
     
--- Content/Script/GHGame/Monster/Task/BT_MoveLocation.lua
+++ Content/Script/GHGame/Monster/Task/BT_MoveLocation.lua
@@ -13,9 +13,9 @@ local BT_MoveLocation = Class()
 --需要主动调用 self:FinishExecute(bSuccess)或者self:FinishAbort()
 
  function BT_MoveLocation:ReceiveExecuteAI(OwnerController, ControlledPawn)
-     local ActorLocation = ControlledPawn:K2_GetActorLocation()
+     local ActorLocation = UE.FVector(4300,2660,88)
      local RandomLocation = UE.FVector()
-     UE.UNavigationSystemV1.K2_GetRandomLocationInNavigableRadius(self,ActorLocation,RandomLocation,500)
+     UE.UNavigationSystemV1.K2_GetRandomLocationInNavigableRadius(self,ActorLocation,RandomLocation,1000)
      UE.UBTFunctionLibrary.SetBlackboardValueAsVector(self,self.MoveLocation,RandomLocation)
      self:FinishExecute(true)
  end
